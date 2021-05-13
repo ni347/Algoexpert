bed03d866b9428794f1368c6b669c46b72f89fce
ni347 <nkv300999@gmail.com>
2021-05-13 17:09:51 +0530

    button

---
+++ Algoexpert/script.js
@@ -0,0 +1,16 @@
+function algo()
+{
+    // let ele = document.getElementById("team");
+    let element = document.getElementById("nikhil");
+    if(element.classList.contains("buttonbb"))
+      element.classList.contains("buttonbb");
+    element.classList.add("buttonbb");  
+}
+
+function algoexp()
+{
+  let element = document.getElementById("kiran");
+  if(element.classList.contains("buttonbb"))
+    element.classList.contains("buttonbb");
+  element.classList.add("buttonbb");
+}
